6453283cb0bedebc0ffc255177e0be7f40c0d725
evgenkaban <k0990245853@gmail.com>
2017-03-22 14:33:14 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,149 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser');
+const port = process.env.PORT || 8080;
+const fs = require('fs');
+var request = require('request');
+const MongoClient    = require('mongodb').MongoClient;
+const db             = require('./config/db');
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var myJSONObject = { 
+    "Host": "api.cognitive.microsoft.com",
+    "Ocp-Apim-Subscription-Key": "b2719d54a3ac423d85b138cfac4d1809"
+}; 
+
+function search_result_obj(body){
+    var arr = body.value;
+    var result_arr = [];
+    //console.log(body.queryExpansions[0])
+    for (var i = 0; i < arr.length; i++){
+        //console.log(arr[i].name)
+        var obj =
+            {
+                url: arr[i].contentUrl,
+                snippet: arr[i].name,
+                thumbnail: arr[i].thumbnailUrl,
+                context: arr[i].hostPageDisplayUrl
+            };
+        result_arr.push(obj);
+    }
+    return result_arr;
+}
+
+app.get('/latest', function (req, res, next) {
+    console.log("  Hello, please use path:  'https://myurlshort.herokuapp.com/latest/imagesearch'  or   'https://myurlshort.herokuapp.com/api/imagesearch/hotdog%20offset=20'   ")
+    res.send("  Hello, please use path:  'https://myurlshort.herokuapp.com/latest/imagesearch'  or   'https://myurlshort.herokuapp.com/api/imagesearch/hotdog%20offset=20'   ");
+
+}) 
+
+app.get('/', function (req, res, next) {
+    console.log("  Hello, please use path:  'https://myurlshort.herokuapp.com/latest/imagesearch'  or   'https://myurlshort.herokuapp.com/api/imagesearch/hotdog%20offset=20'   ")
+    res.send("  Hello, please use path:  'https://myurlshort.herokuapp.com/latest/imagesearch'  or   'https://myurlshort.herokuapp.com/api/imagesearch/hotdog%20offset=20'   ");
+})
+
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  
+  app.get('/latest/imagesearch', function (req, res, next) {
+      
+    var result_arr = [];  
+    database.collection('notes').find({
+      }).sort({when: -1}).limit(10)
+.toArray(function(err, docs) {
+        if (err) throw err;
+        for(var i in docs){
+            var obj = { term: docs[i].term, when: docs[i].when };
+            result_arr.push( obj )
+        }
+        console.log('mongo latest ')
+        console.log(result_arr)
+        res.send(result_arr);
+    }) 
+  }) 
+  
+})
+
+
+
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  app.get('/api/imagesearch/:id', function (req, res, next) {
+    var term = req.params.id.split(' ')[0];
+    var jsonDate = (new Date()).toJSON();
+
+    var note = { term: term, when: jsonDate };
+    console.log(note)
+    
+    database.collection('notes').insert(note, (err, result) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        //res.send(result);
+        next();
+      }
+    })
+  })              
+})
+
+
+
+app.get('/api/imagesearch/:id', function (req, res, next) {
+    var loking_item = req.params.id.split(' ')[0];
+    var items_number = req.params.id.split(' ')[1].split('=')[1];
+    var url = "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q="+loking_item+"&count="+items_number+"&offset=0";
+    request({
+        url: url, //URL to hit
+        method: 'GET', //Specify the method
+        headers: { //We can define headers too
+            'Host': 'api.cognitive.microsoft.com',
+            'Ocp-Apim-Subscription-Key': 'b2719d54a3ac423d85b138cfac4d1809'
+    }
+    }, function(error, response, body){
+        if(error) {
+            console.log(error);
+        } else {
+            console.log('Hello World!');
+            var obj_from_api = search_result_obj(JSON.parse(body));
+            res.send(obj_from_api);
+            next();
+        }
+    })
+    
+    
+
+});
+
+
+
+app.listen(port, function () {
+  console.log("Server has started.")
+})
+
+
+
+/*
+key for bing:  b2719d54a3ac423d85b138cfac4d1809
+npm run dev
+
+request({
+    url: 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=cats&count=4&offset=0', //URL to hit
+    //qs: {from: 'blog example', time: +new Date()}, //Query string data
+    method: 'GET', //Specify the method
+    headers: { //We can define headers too
+        'Host': 'api.cognitive.microsoft.com',
+        'Ocp-Apim-Subscription-Key': 'b2719d54a3ac423d85b138cfac4d1809'
+    }
+}, function(error, response, body){
+    if(error) {
+        console.log(error);
+    } else {
+        console.log('Hello World!');
+        res.send(body)
+    }
+
+*/
